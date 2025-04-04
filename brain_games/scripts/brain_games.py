a8bc193c2a3e64d0b27ec929e25caa953330e6a1
qwert3001 <qwert3001nik@gmail.com>
2025-04-04 20:02:37 +0300

    Aded game_gcd.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -3,6 +3,7 @@ from brain_games.cli import welcome_user
 # from brain_games.scripts.brain_even import brain_even
 from brain_games.scripts.games.game_calculator import game_calculator
 from brain_games.scripts.games.game_even import game_even
+from brain_games.scripts.games.game_gcd import game_gcd
 
 
 def greet():
@@ -12,8 +13,9 @@ def greet():
 def main():
     greet()
     user_name = welcome_user()
-    game_even(user_name)
-    game_calculator(user_name)
+    #game_even(user_name)
+    #game_calculator(user_name)
+    game_gcd(user_name)
     
 
 if __name__ == "__main__":
