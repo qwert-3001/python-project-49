ea93cac5f42c8252cec46fab42b9ae6f6e658c11
qwert3001 <qwert3001nik@gmail.com>
2025-04-06 18:43:33 +0300

    Revert "Change const name"
    This reverts commit 73339077d927d8d5395e2bc5896c8369164b0620.

--- brain_games/scripts/games/game_prime.py
+++ brain_games/scripts/games/game_prime.py
@@ -5,11 +5,11 @@ from brain_games.scripts.game_engine import run_game
 
 def generate_prime_question():
     
-    simple_number = (2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53)
+    SIMPLE_NUMBER = (2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53)
     answer = ''
 
     question = random.randint(1, 54)
-    if question in simple_number:
+    if question in SIMPLE_NUMBER:
         answer = 'yes'
     else:
         answer = 'no'
